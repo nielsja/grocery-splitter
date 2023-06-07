2ad9bb0f076db681b40a24f8c142eae9722e44ee
Jennifer Nielsen <nielsja13@gmail.com>
2023-06-07 12:35:11 -0500

    Add app-layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppLayoutComponent,
   ],
   imports: [
     BrowserModule,
