f679ff750c2b226be918315203be58e4dac5140c
Jennifer Nielsen <nielsja13@gmail.com>
2023-06-08 09:56:00 -0500

    have line items radio buttons working (cleaned up part 2)

--- src/app/line-item/line-item.component.ts
+++ src/app/line-item/line-item.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Parties } from '../shared/Parties';
-import { SplitBy } from '../shared/SplitByEnum';
 
 @Component({
   selector: 'line-item',
@@ -10,9 +8,6 @@ import { SplitBy } from '../shared/SplitByEnum';
 })
 export class LineItemComponent implements OnInit, OnChanges {
   @Input() itemNumber = '';
-  amount = 0;
-  parties = Parties;
-  splitBy = '';
 
   formGroup = new FormGroup({
     splitByRadio: new FormControl(),
