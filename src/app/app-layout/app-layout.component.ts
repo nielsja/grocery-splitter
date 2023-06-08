f7bd99f8b9765ab1fdb5ef73b445350344e1748d
Jennifer Nielsen <nielsja13@gmail.com>
2023-06-08 10:51:37 -0500

    set up line items to emit item amount and split person on change

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LineItemComponent } from '../line-item/line-item.component';
+import { ILineItemUpdateEventPayload, LineItemComponent } from '../line-item/line-item.component';
 
 @Component({
   selector: 'app-layout',
@@ -7,16 +7,31 @@ import { LineItemComponent } from '../line-item/line-item.component';
   styleUrls: ['./app-layout.component.scss']
 })
 export class AppLayoutComponent implements OnInit {
-  lineItems: LineItemComponent[] = []
+  lineItems = [1, 2, 3]
+  lineItemEventPayload: ILineItemUpdateEventPayload = { itemNumber: -1, itemAmount: -1, splitBy: -1 };
+  lineItem1Data: ILineItemUpdateEventPayload = { itemNumber: 1, itemAmount: -1, splitBy: -1 };
+  lineItem2Data: ILineItemUpdateEventPayload = { itemNumber: 2, itemAmount: -1, splitBy: -1 };
+  lineItem3Data: ILineItemUpdateEventPayload = { itemNumber: 3, itemAmount: -1, splitBy: -1 };
 
   constructor() { }
 
   ngOnInit(): void {
-    this.lineItems = [
-      new LineItemComponent(),
-      new LineItemComponent(),
-      new LineItemComponent()
-    ];
+
   }
 
+  lineItemEvent(payload: ILineItemUpdateEventPayload) {
+    this.lineItemEventPayload = payload;
+    switch (payload.itemNumber) {
+      case 1:
+        this.lineItem1Data = payload;
+        break;
+      case 2:
+        this.lineItem2Data = payload;
+        break;
+      case 3:
+        this.lineItem3Data = payload;
+        break;
+
+    }
+  }
 }
--- src/app/line-item/line-item.component.ts
+++ src/app/line-item/line-item.component.ts
@@ -1,6 +1,11 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, Output, OnChanges, OnInit, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+export interface ILineItemUpdateEventPayload {
+  itemNumber: number;
+  itemAmount: number;
+  splitBy: number;
+}
 @Component({
   selector: 'line-item',
   templateUrl: './line-item.component.html',
@@ -8,12 +13,26 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class LineItemComponent implements OnInit, OnChanges {
   @Input() itemNumber = '';
+  @Output() lineItemUpdateEvent = new EventEmitter<ILineItemUpdateEventPayload>();
+  @Output() splitByEvent = new EventEmitter<number>();
+  @Output() itemAmount = new EventEmitter<number>();
 
   formGroup = new FormGroup({
     splitByRadio: new FormControl(),
     itemAmount: new FormControl()
   })
 
+  updateLineItem() {
+    this.lineItemUpdateEvent.emit({
+      itemNumber: Number(this.itemNumber),
+      itemAmount: this.formGroup.controls['itemAmount'].value,
+      splitBy: this.formGroup.controls['splitByRadio'].value
+    });
+  }
+
+  splitByClicked(splitBy: number): void {
+    this.splitByEvent.emit(splitBy);
+  }
 
   constructor() { }
 
